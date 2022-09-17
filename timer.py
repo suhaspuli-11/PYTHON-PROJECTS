9c7c11994bf83127a47c2314ec8d96a42e467430
suhaspuli-11 <102015184+suhaspuli-11@users.noreply.github.com>
2022-09-17 16:36:33 +0530

    final

--- timer.py
+++ timer.py
@@ -4,9 +4,9 @@ import time
 
 def countdown(t):
     while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
+        minutes, seconds = divmod(t, 60)
+        timer2 = f"{minutes:{0}{2}}:{seconds:{0}{2}}"
+        print(timer2, end="\r")
         time.sleep(1)
         t -= 1
     print('It\'s time!')
